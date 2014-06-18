db3d7c135ac391494a2eac4e006368296c3132fd
oscaraca <oscar.chavarriaga@correounivalle.edu.co>
2014-06-17 21:33:01 -0500

    web service android actualizado

--- ProjectWWW/androidService/servicioAndroid.php
+++ ProjectWWW/androidService/servicioAndroid.php
@@ -1,4 +1,20 @@
 <?php
-$data = array('name' => 'Froyo', 'version' => 'Android 2.2'); 
-print (json_encode($data)); 
-?> 
+error_reporting(0);
+
+//Decodificamos los datos
+$se = json_decode(stripslashes($_POST["key"]));
+//Mostramos los datos
+//echo $se->codigoPedido."modif\n";
+$codigo_pedido = $se->codigoPedido;
+
+include('ConexionDB.php');
+$query = "SELECT estado FROM pedido WHERE consecutivo=$se->codigoPedido";
+$result = pg_query(getDB(), $query);
+$row = pg_fetch_array($result);
+$var = $row['estado'];
+if (pg_num_rows($result) == 0) {
+    echo "noexiste";
+} else {
+    echo $var;
+}
+?>
